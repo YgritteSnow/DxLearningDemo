6e0b8b806e7a06e22e630040751d08be3ca1a879
YgritteSnow <773963715@qq.com>
2016-05-18 21:19:57 +0800

    设置ui位置（有点不准，可能有问题）

--- ui/ui_base.cpp
+++ ui/ui_base.cpp
@@ -7,8 +7,8 @@ DWORD UIBase::UIVertex::FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1;
 
 void UIBase::Config()
 {
-	SetSize(2.0f, 1.0f);
-	SetPos(1.f, 1.0f);
+	SetSize(0.5f, 0.5f);
+	SetPos(0.5f, 0.5f);
 }
 
 void UIBase::PreRender( LPDIRECT3DDEVICE9 device )
@@ -167,19 +167,29 @@ void UIBase::RecalMatrix()
 	
 	D3DXMATRIX trans_mat;
 
-	//第一种，平移一丢丢
-	D3DXMatrixTranslation( &trans_mat, 0.f, 0.f, 0.1f );
+	//第一种，平移一丢丢。。。
+	//D3DXMatrixTranslation( &trans_mat, 0.f, 0.f, 0.1f );
+	//
 	
 	//第二种，自己做个投影矩阵。。。
 	//D3DXMatrixIdentity( &trans_mat );
+	//trans_mat._11 = g_screen_real_width * 0.5f;
+	//trans_mat._22 = g_screen_real_height * 0.5f;
 	//trans_mat._33 = 1.1f;
 	//trans_mat._34 = 1.f;
-	//trans_mat._43 = 0.0f;
 	//trans_mat._44 = 0.0f;
 
 	//第三种，用函数做投影矩阵。。。
-	//D3DXMatrixOrthoLH( &trans_mat, g_screen_real_width * 0.9f, g_screen_real_height * 0.9f, 1 + 0.01f, 1 );
+	//D3DXMatrixOrthoLH( &trans_mat, g_screen_real_width * 1.f, g_screen_real_height * 0.8f, 1 + 0.01f, 1 );
 	//D3DXMatrixInverse( &trans_mat, NULL, &trans_mat );
+
+	//第四种，要加上位置属性了，还是自己做矩阵吧~
+	D3DXMatrixIdentity( &trans_mat );
+	trans_mat._11 = GetScaleH();
+	trans_mat._22 = GetScaleV();
+	trans_mat._41 = GetPosH();
+	trans_mat._42 = GetPosV();
+	
 
 	D3DXMatrixMultiply( &m_matrix, &trans_mat, inv_view_mat );
 
